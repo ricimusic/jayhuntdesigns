b7618805f9dc3f3cac29d190768a66e78e5f4b90
Shenole Latimer <shenole@gmail.com>
2017-10-08 22:36:35 -0400

    Adjusted timer per jasmine request

---
+++ scripts/script-root.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	setBindings();
+});
+
+function setBindings() {
+
+    $(".nav a").click(function(e) {
+    	e.preventDefault();
+        var sectionID = e.currentTarget.id + "Section";
+
+            $("html body").animate({
+                scrollTop: $("#" + sectionID).offset().top
+
+            }, 1000)
+    })
+}
